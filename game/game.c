9378b93819be3ceed561c9cc61f1dc32c24d5f4a
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-01 17:09:01 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:09:01 up  1:12,  3 users,  load average: 0.13, 0.09, 0.12 7f2ca8b21713ba072d60cac4f1276586cd58a18b

--- game/game.c
+++ game/game.c
@@ -1,8 +1,11 @@
 int printk(const char *fmt, ...);
 void testprintk();
+void init_vga();
+
 int game_init(){
 	printk("@_@\n");
-	testprintk();
+//	testprintk();
+	init_vga();
 	while(1);
 	return 0;
 }
