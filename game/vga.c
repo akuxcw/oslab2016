c418ab734cda1c74ff175d14dfe38ea727aae032
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-01 17:59:36 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:59:36 up  2:02,  3 users,  load average: 0.16, 0.19, 0.16 b7a29d769cb3b26e5eeb770cdc9a999a305c425d

--- game/vga.c
+++ game/vga.c
@@ -4,5 +4,5 @@ unsigned char * color_buffer = (unsigned char*)0xA0000;
 #define color(x,y) color_buffer[x*COL+y]
 void init_vga(){
 	int i;
-	for(i = 0; i < 0x1000000; i ++) color_buffer[i] = 0xf;
+	for(i = 0; i < 0x10000000; i ++) color_buffer[i] = 0xf;
 }
