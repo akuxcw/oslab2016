8136176cb530809e733441fff4de40a5099c1032
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-01 19:14:57 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:14:57 up  3:18,  3 users,  load average: 0.07, 0.06, 0.05 ef6b59195a58dd96cef940353dfe9bf48f7b2b30

--- game/vga.c
+++ game/vga.c
@@ -12,5 +12,5 @@ InfoBlock *vbeinfo = (InfoBlock *) 0x9000;
 void init_vga(){
 	int i;
 	for(i = 0; i < COL*ROW; i ++) color_buffer[i] = 0xf;
-	printk("%x\n", 1);//vbeinfo->physbase);
+	printk("%x\n", vbeinfo->physbase);
 }
