bc93077195a5959c5913fd170aba8f3ce8b682ed
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-01 21:26:52 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:26:52 up  5:30,  4 users,  load average: 0.09, 0.20, 0.17 eff9d172ba395efe12e352d06b16cd28206d144d

---
+++ game/data/jpg1.d
@@ -0,0 +1,1 @@
+game/data/jpg1.o: game/data/jpg1.c /usr/include/stdc-predef.h
--- game/vga.c
+++ game/vga.c
@@ -1,20 +1,14 @@
 #include <inc/types.h>
 #include "vga.h"
 
-#define VGA_ADDR 0xfc000000
-#define ROW 600
-#define COL 800
-unsigned char * color_buffer = (unsigned char*)VGA_ADDR;
-#define color(x,y) color_buffer[x*COL+y]
-
 extern int jpg[600*800];
 int printk(const char * fmt, ...);
 //int a = 0;
-const InfoBlock *vbeinfo = (InfoBlock *)0xa01;
 void init_vga(){
+	color_buffer = (RGB *) VGA_ADDR;
 	int i;
-	for(i = 0; i < 3*COL*ROW; i ++) {
-		switch (i%3){
+	for(i = 0; i < V_COL * V_ROW; i ++) {
+/*		switch (i%3){
 			case 0 : color_buffer[i] = jpg[i/3] & 0xff;
 				break;
 			case 1 : color_buffer[i] = (jpg[i/3] >> 8) & 0xff;
@@ -22,7 +16,12 @@ void init_vga(){
 			case 2 : color_buffer[i] = (jpg[i/3] >> 16) & 0xff;
 				break;
 		}
+*/
+		color_buffer[i].val = jpg[i];
 	}
-	printk("%x\n", vbeinfo->physbase);
-	printk("%x\n", vbeinfo);
+//	while(1) {
+//		for()
+//	}
+//	printk("%x\n", vbeinfo->physbase);
+//	printk("%x\n", vbeinfo);
 }
