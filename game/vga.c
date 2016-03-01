e0d2fdb7b10b08322d760e7a69a5cdb35675afe1
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-01 17:48:07 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:48:07 up  1:51,  3 users,  load average: 0.00, 0.07, 0.11 f1bd319e7fce9134923e0aa52b6434c62d475fd0

--- game/vga.c
+++ game/vga.c
@@ -1,6 +1,6 @@
 #define ROW 600
 #define COL 8000
-unsigned char * color_buffer = (unsigned char*)0x10000;
+unsigned char * color_buffer = (unsigned char*)0xA0000;
 #define color(x,y) color_buffer[x*COL+y]
 void init_vga(){
 	int i;
