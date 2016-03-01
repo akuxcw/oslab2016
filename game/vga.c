115f8aede2ae433eaf8f42cb4dee63f118dcdc6a
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-01 20:27:23 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:27:22 up  4:30,  4 users,  load average: 0.14, 0.36, 0.42 6cccfcf3c86b2ad21d094ece5054eb7b8e4fca8f

--- game/vga.c
+++ game/vga.c
@@ -7,7 +7,7 @@
 unsigned char * color_buffer = (unsigned char*)VGA_ADDR;
 #define color(x,y) color_buffer[x*COL+y]
 
-extern int jpg[220*293];
+extern int jpg[600*800];
 int printk(const char * fmt, ...);
 //int a = 0;
 const InfoBlock *vbeinfo = (InfoBlock *)0xa01;
@@ -15,11 +15,11 @@ void init_vga(){
 	int i;
 	for(i = 0; i < 3*COL*ROW; i ++) {
 		switch (i%3){
-			case 0 : color_buffer[i] = 0xdb;
+			case 0 : color_buffer[i] = jpg[i] & 0xff;
 				break;
-			case 1 : color_buffer[i] = 0x93;
+			case 1 : color_buffer[i] = (jpg[i] >> 8) & 0xff;
 				break;
-			case 2 : color_buffer[i] = 0x70;
+			case 2 : color_buffer[i] = (jpg[i] >> 16) & 0xff;
 				break;
 		}
 	}
