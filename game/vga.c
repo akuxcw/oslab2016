14bc99a8f51477305ce3be30a09ef81697de6bda
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-01 21:29:26 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:29:26 up  5:32,  4 users,  load average: 0.30, 0.26, 0.20 4d1a29c5cd63e97cf90f5277c71af5c40de8e38

--- game/vga.c
+++ game/vga.c
@@ -8,16 +8,10 @@ void init_vga(){
 	color_buffer = (RGB *) VGA_ADDR;
 	int i;
 	for(i = 0; i < V_COL * V_ROW; i ++) {
-/*		switch (i%3){
-			case 0 : color_buffer[i] = jpg[i/3] & 0xff;
-				break;
-			case 1 : color_buffer[i] = (jpg[i/3] >> 8) & 0xff;
-				break;
-			case 2 : color_buffer[i] = (jpg[i/3] >> 16) & 0xff;
-				break;
-		}
-*/
-		color_buffer[i].val = jpg[i];
+		color_buffer[i].R = jpg[i] & 0xff;
+		color_buffer[i].G = (jpg[i] >> 8) & 0xff;
+		color_buffer[i].B = (jpg[i] >> 16) & 0xff;
+//		color_buffer[i].val = jpg[i];
 	}
 //	while(1) {
 //		for()
