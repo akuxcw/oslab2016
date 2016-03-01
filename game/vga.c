870765bc801257cb2ce0b58bd6984e0f1a258186
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-01 19:11:34 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:11:34 up  3:14,  3 users,  load average: 0.01, 0.05, 0.06 e305ba9dec07d237b19066737fefafcb7057402c

--- game/vga.c
+++ game/vga.c
@@ -7,10 +7,10 @@ unsigned char * color_buffer = (unsigned char*)0xA0000;
 #define color(x,y) color_buffer[x*COL+y]
 
 int printk(const char *fmt, ...);
-
-InfoBlock vbeinfo;
+int a = 0;
+//InfoBlock *vbeinfo = ;
 void init_vga(){
 	int i;
 	for(i = 0; i < COL*ROW; i ++) color_buffer[i] = 0xf;
-	printk("%x\n", &vbeinfo);
+	printk("%x\n", &a);
 }
