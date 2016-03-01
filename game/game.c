9e288cc7b2f10cb7c9db15c22ef65b4a054dcfdc
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-01 16:30:08 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:30:08 up 33 min,  3 users,  load average: 0.02, 0.06, 0.06 6cefdf7f90405bbe318a078113062b082c089696

--- game/game.c
+++ game/game.c
@@ -1,8 +1,8 @@
-int cprintf(const char *fmt, ...);
-int printk(const char *fmt, ...);
+//int cprintf(const char *fmt, ...);
+//int printk(const char *fmt, ...);
 
 int game_init(){
-	printk("@_@\n");
+//	printk("@_@\n");
 	while(1);
 	return 0;
 }
