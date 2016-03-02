8d3128f2c9e6a4cef18cab31ff22dfc1b1d8c4b1
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-02 16:20:17 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:20:17 up 26 min,  3 users,  load average: 0.64, 0.54, 0.43 fe47c598d003fe2304f066fdce6d694c74510d2

--- game/vga.c
+++ game/vga.c
@@ -26,7 +26,7 @@ void init_vga(){
 		
 		for(i = x; i < x + k; ++ i)
 			for(j = y; j < y + k; ++ j)
-				toColor(color(i,j),0xffff00);
+				toColor(color(i,j),0xff00ff);
 	}
 //	printk("%x\n", VbeInfo->physbase);
 //	printk("%x\n", VbeInfo);
