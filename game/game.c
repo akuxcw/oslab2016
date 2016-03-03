a1c7e1b47feb9e26de84d6705ff06ddd567c0ac1
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-03 20:04:48 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:04:48 up  3:52,  5 users,  load average: 0.18, 0.36, 0.67 6ea04f0f59d5bc4cea360167d0315b78dc2ed22

--- game/game.c
+++ game/game.c
@@ -4,6 +4,7 @@ void init_vga();
 void init_i8259();
 void init_timer();
 void init_serial();
+void init_idt();
 
 void set_timer_intr_handler( void (*ptr)(void));
 void set_kbd_intr_handler( void (*ptr)(int));
@@ -15,10 +16,12 @@ int game_init(){
 	init_timer();
 	init_i8259();
 	init_serial();
+	init_idt();
 	set_timer_intr_handler(timer_event);
 	set_kbd_intr_handler(kbd_event);
 
 	printk("@_@\n");
+	asm volatile("sti");
 	init_vga();
 	while(1);
 	return 0;
