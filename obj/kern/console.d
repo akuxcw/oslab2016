ce6a0f869c8b1bbab2b88c92c84d94f2476811e8
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-03 16:48:32 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:48:32 up 36 min,  3 users,  load average: 0.04, 0.04, 0.05 40658e187a5b426b242d41d153bb6541cfce4333

---
+++ obj/kern/console.d
@@ -0,0 +1,5 @@
+obj/kern/console.o: kern/console.c /usr/include/stdc-predef.h inc/x86.h \
+ inc/types.h inc/memlayout.h inc/../inc/types.h inc/../inc/mmu.h \
+ inc/../inc/../inc/types.h inc/kbdreg.h inc/string.h inc/assert.h \
+ inc/../inc/stdio.h inc/../inc/../inc/stdarg.h kern/console.h \
+ kern/../inc/types.h
