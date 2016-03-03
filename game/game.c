ef64cfab66df7402351871cc66847d85fb6baa58
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-03 19:37:27 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:37:27 up  3:25,  4 users,  load average: 1.77, 1.31, 0.65 8d39814a8a55bcc4216f459b2dc747765b18b42b

--- game/game.c
+++ game/game.c
@@ -5,7 +5,7 @@ void init_i8259();
 void init_timer();
 void init_serial();
 
-/*
+
 int syscall(int id, ...);
 static void add_irq_handle(int irq, void *handler) {
 	syscall(0, irq, handler);
@@ -13,12 +13,12 @@ static void add_irq_handle(int irq, void *handler) {
 
 void timer_event(void); 
 void kbd_event(void);
-*/
+
 int game_init(){
 	init_timer();
 	init_i8259();
 	init_serial();
-//	add_irq_handle(0, timer_event);
+	add_irq_handle(0, timer_event);
 //	add_irq_handle(0, kbd_event);
 
 	printk("@_@\n");
