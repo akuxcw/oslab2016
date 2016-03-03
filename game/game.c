a656441407d65ab333f3c745bcda1dc9be47f22b
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-03 19:31:01 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:31:01 up  3:19,  4 users,  load average: 0.00, 0.07, 0.10 e3746e5ca83cba7bc7c2c1171282847a8824b77c

--- game/game.c
+++ game/game.c
@@ -2,6 +2,7 @@ int printk(const char *fmt, ...);
 void testprintk();
 void init_vga();
 void init_i8259();
+void init_timer();
 void init_serial();
 int syscall(int id, ...);
 
@@ -9,21 +10,17 @@ static void add_irq_handle(int irq, void *handler) {
 	syscall(0, irq, handler);
 }
 
-volatile int tick = 0;
-
-void timer_event(void) {
-	tick ++;
-}
+void timer_event(void); 
 void kbd_event(void);
 
-
 int game_init(){
-	printk("@_@\n");
+	init_timer();
 	init_i8259();
 	init_serial();
 	add_irq_handle(0, timer_event);
 	add_irq_handle(0, kbd_event);
 
+	printk("@_@\n");
 	init_vga();
 	while(1);
 	return 0;
---
+++ obj/kern/timer.d
@@ -0,0 +1,2 @@
+obj/kern/timer.o: kern/timer.c /usr/include/stdc-predef.h inc/types.h \
+ inc/x86.h inc/types.h
