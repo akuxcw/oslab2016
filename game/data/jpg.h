2a68289b82a8f52ca49eacd62b9b7822eaa7b275
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-05 11:55:32 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  11:55:32 up  1:50,  5 users,  load average: 0.14, 0.15, 0.14 a2279fc9f765d5f3a6551b73d6364bc0fce73edb

--- game/data/jpg.h
+++ game/data/jpg.h
@@ -5,11 +5,14 @@ typedef struct jpgtype {
 	int arr[600*800];
 } jpg;
 
+extern int Property[700][900];
+
 static inline void 
-Displayjpg(int x, int y, jpg* graph) {
+Displayjpg(int x, int y, jpg* graph, int p) {
 	int i, j;
 	for(i = 0; i < graph->height; ++ i)
 		for(j = 0; j < graph->length; ++ j) {
 			toColor(color(x + i, y + j), graph->arr[i * graph->length + j]);
+			Property[x + i][y + j] = p;
 		}
 }
--- game/game.c
+++ game/game.c
@@ -3,54 +3,66 @@
 #include <inc/stdio.h>
 #include "data/jpg.h"
 
-#define goal 600*800
-
 extern jpg Basic2;
 extern jpg Basic;
 extern jpg GreenBlock;
 
-int cons_getc(void);
 bool query_key(int);
 void Delay(int t);
+
 bool v[600][800];
+int Property[700][900];
+int ans, goal;
+int Xnow, Ynow, Width, Vy, Vx, Vsx, Jump;
+
+void init_game() {
+	color_buffer = (RGB *) VGA_ADDR;
+	Displayjpg(0, 0, &Basic, 0);
+	Displayjpg(200, 10, &GreenBlock, 1);
+	memset(v, 0, sizeof v);	
+	memset(v, 0, sizeof v);	
+	ans = 0; goal = 600 * 800; 
+	Xnow = 550, Ynow = 10, Width = 50, Vy = 1;
+	Vx = 0;
+	Vsx = 110;
+	Jump = 0;
+	
+}
 
 void game(){
-	color_buffer = (RGB *) VGA_ADDR;
-	int i, j;
 START:
-/*
-	for(i = 0; i < V_COL * V_ROW; i ++) {
-		toColor(color_buffer[i], Basic.arr[i]);
-	}
-*/
-	Displayjpg(0, 0, &Basic);
-	Displayjpg(200, 10, &GreenBlock);
-	memset(v, 0, sizeof v);
-	int ans = 0; 
-
-	int x = 550, y = 10, k = 50, d = 1;
-	int Vx = 0;
-	int Vsx = 110;
-	int jump = 0;
+	init_game();
+	int i, j;
 	while(1) {
-		for(i = x; i < x + k; ++ i)
-		  	for(j = y; j < y + k; ++ j) {
+		for(i = Xnow; i < Xnow + Width; ++ i)
+		  	for(j = Ynow; j < Ynow + Width; ++ j) {
 			  	toColor(color(i,j), Basic2.arr[i * V_COL + j]);
 				if(i >= 0 && i < V_ROW && j >= 0 && j < V_COL) {
 					if(!v[i][j]) ans ++, v[i][j] = true;
 				}
 			}
 		if (ans == goal) goto START;
-		if (jump) {
-			x += Vx/10;
-			if (Vx != Vsx) Vx += 1; else jump = false;
+		if (Jump) {
+			int tmp = Xnow + Vx/10;
+			if(Vx > 0) {
+				if(Property[tmp + Width][Ynow] == 1 || Property[tmp + Width][Ynow + Width] == 1) {
+					Jump = false;
+					while(Property[tmp + Width][Ynow] == 1 || Property[tmp + Width][Ynow + Width] == 1) tmp --;
+				}
+			} else {
+				if(Property[tmp][Ynow] == 1 || Property[tmp][Ynow + Width] == 1) {
+					Vx = - Vx;
+					while(Property[tmp][Ynow] == 1 || Property[tmp][Ynow + Width] == 1) tmp ++;
+				}
+			}
+			Xnow = tmp;
+			if (Jump) Vx += 1;
 		}
-		if (query_key('w' - 'a') && !jump) Vx = -Vsx, jump = true;
-//		if (query_key('s' - 'a') && x < V_ROW - k) x += d;
-		if (query_key('a' - 'a') && y > 0) y -= d;
-		if (query_key('d' - 'a') && y < V_COL - k) y += d;
-		for(i = x; i < x + k; ++ i)
-			for(j = y; j < y + k; ++ j)
+		if (query_key('w' - 'a') && !Jump) Vx = -Vsx, Jump = true;
+		if (query_key('a' - 'a') && Ynow > 0) Ynow -= Vy;
+		if (query_key('d' - 'a') && Ynow < V_COL - Width) Ynow += Vy;
+		for(i = Xnow; i < Xnow + Width; ++ i)
+			for(j = Ynow; j < Ynow + Width; ++ j)
 				toColor(color(i,j),0x00ff);
 		Delay(5);
 	}
