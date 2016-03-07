f5f696d24122473e510c8df4bde9238acf980d5d
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-07 17:22:17 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:22:17 up  1:43,  8 users,  load average: 0.11, 0.12, 0.22 d419a1e8538079286b39b307d1d74015132d2f99

--- game/device/video.c
+++ game/device/video.c
@@ -11,12 +11,6 @@ extern int Property[700][900];
 
 void process_video() {
 	int i, j;
-	for(i = 0; i < 700; ++ i) {
-		for(j = 0; j < 900; ++ j) {
-			Property[i][j] = 1;
-		}
-	}
-	Displayjpg(0, 0, &Basic, SKY);
 	for(i = 100; i < 600; i += 100) 
 	  	for(j = 0; j < 800; j += 100)
 			Displayjpg(i, j, &GreenBlock, GROUND);
