09625b81fdff23f3f70d56f51d5aa7bea8698b92
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-07 17:27:10 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:27:10 up  1:48,  8 users,  load average: 0.41, 0.30, 0.26 24e3f8be979fc783224ed40fc1c73013980bd5c0

--- game/device/video.c
+++ game/device/video.c
@@ -13,13 +13,13 @@ extern int Xnow, Ynow, Width;
 
 void process_video() {
 	int i, j;
-	for(i = 100; i < 600; i += 100) 
-	  	for(j = 0; j < 800; j += 100)
-			Displayjpg(i, j, &GreenBlock, GROUND);
-	Displayjpg(10, 400, &GoldenBlock, SKY);
 	for(i = Xnow; i < Xnow + Width; ++ i)
 		for(j = Ynow; j < Ynow + Width; ++ j)
 			toColor(color(i,j),0x00ff);
 
+	for(i = 100; i < 600; i += 100) 
+	  	for(j = 0; j < 800; j += 100)
+			Displayjpg(i, j, &GreenBlock, GROUND);
+	Displayjpg(10, 400, &GoldenBlock, SKY);
 }
 
