fd7fb170ac22bc289621fc5252d8a407e202f1c8
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-09 17:22:08 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:22:08 up  1:13,  5 users,  load average: 0.08, 0.13, 0.21 cb76f6c7c5697525b688c870513434a478c7d383

--- lib/string.c
+++ lib/string.c
@@ -1,6 +1,6 @@
 // Basic string routines.  Not hardware optimized, but not shabby.
 
-#include "../inc/string.h"
+#include <inc/string.h>
 
 // Using assembly for memset/memmove
 // makes some difference on real hardware,
