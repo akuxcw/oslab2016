a14a53678eec0144ce3f7be6f292a55ce9f3fb88
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-09 16:24:04 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:24:04 up 15 min,  3 users,  load average: 0.03, 0.21, 0.22 3c4f5ccce785771d8330f64afeb9c28d1b656106

--- game/device/video.c
+++ game/device/video.c
@@ -17,10 +17,10 @@ void process_video() {
 		for(j = Ynow; j < Ynow + Width; ++ j)
 			toColor(color(i,j),0x00ff);
 
-/*	for(i = 100; i < 600; i += 100) 
+	for(i = 100; i < 600; i += 100) 
 	  	for(j = 0; j < 800; j += 100)
 			Displayjpg(i, j, &GreenBlock, GROUND);
 	Displayjpg(10, 400, &GoldenBlock, SKY);
-*/
+
 }
 
