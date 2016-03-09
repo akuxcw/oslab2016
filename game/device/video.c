8112fac8a37fc71227a75499c34cdc28d0ca9a20
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-09 16:52:15 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:52:15 up 43 min,  5 users,  load average: 0.28, 0.23, 0.23 16f0608f311d3aece11ed424f065c352e3a4237d

--- game/device/video.c
+++ game/device/video.c
@@ -2,25 +2,35 @@
 #include "../vga.h"
 
 enum {SKY, GROUND};
+enum {EMPTY, GREEN, GOLDEN};
 
 extern jpg Basic2;
 extern jpg Basic;
 extern jpg GreenBlock;
 extern jpg GoldenBlock;
 extern int Property[700][900];
+extern int Block[20][100];
 
 extern int Xnow, Ynow, Width;
 
 void process_video() {
 	int i, j;
 	for(i = Xnow; i < Xnow + Width; ++ i)
-		for(j = Ynow; j < Ynow + Width; ++ j)
+		for(j = 400; j < 400 + Width; ++ j)
 			toColor(color(i,j),0x00ff);
 
-/*	for(i = 100; i < 600; i += 100) 
-	  	for(j = 0; j < 800; j += 100)
-			Displayjpg(i, j, &GreenBlock, GROUND);
-	Displayjpg(10, 400, &GoldenBlock, SKY);
-*/
+	for(i = 0; i < 20; ++ i) 
+		for(j = 0; j < 100; ++ j) {
+			if(400 + j * 30 - Ynow < 0 || 400 + j * 30 - Ynow > 770) continue;
+			switch(Block[i][j]) {
+				case GREEN :
+					Displayjpg(i * 30, 400 + j * 30 - Ynow, &GreenBlock, GROUND);
+					break;
+				case GOLDEN :
+					Displayjpg(i * 30, 400 + j * 30 - Ynow, &GoldenBlock, SKY);
+					break;
+			}
+		}
+
 }
 
