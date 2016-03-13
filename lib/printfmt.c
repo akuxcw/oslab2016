1282b49f51f547eef4afa4cd63745a4f9d44e202
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-13 20:23:59 +0800

    >  compile game 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-62-generic #83~14.04.1-Ubuntu SMP Fri Feb 26 22:52:39 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:23:59 up 35 min,  4 users,  load average: 0.35, 0.21, 0.16 81510630525b5653fcef0373ce27be5de1e61bf

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -213,10 +213,10 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 			break;
 
 		// escaped '%' character
-/*		case '%':
+		case '%':
 			putch(ch, putdat);
 			break;
-*/
+
 		// unrecognized escape sequence - just print it literally
 		default:
 			putch('%', putdat);
