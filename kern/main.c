a68670e414d31e920e89f1fc15ffd1e4105a90f8
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-21 21:12:17 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:12:17 up  5:10,  8 users,  load average: 0.11, 0.12, 0.13 adebfc5044196981503692b63c9a40ace238b47c

--- kern/main.c
+++ kern/main.c
@@ -23,7 +23,7 @@ int kern_main() {
 	struct Proghdr *ph, *eph;
 	unsigned char* pa, *i;
 
-	elf = (struct Elf*)106496;
+	elf = (struct Elf*)(100*1024 + 0x10000);
 
 	readseg((unsigned char*)elf, 4096, 0);
 
