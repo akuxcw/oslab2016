b775f340298fcc8b1af5b6cdf8a590d538370275
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-21 21:10:29 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:10:29 up  5:08,  8 users,  load average: 0.04, 0.10, 0.12 8519802ebe89e08ce2f63e8dcdcd404d8af25648

--- kern/main.c
+++ kern/main.c
@@ -1,3 +1,9 @@
+#include <inc/x86.h>
+#include <inc/elf.h>
+
+#define SECTSIZE 512
+
+void readseg(unsigned char *, int, int);
 
 void init_i8259();
 void init_timer();
@@ -5,7 +11,6 @@ void init_serial();
 void init_idt();
 
 void init_palette();
-//void game_main();
 
 int kern_main() {
 	init_timer();
@@ -13,6 +18,53 @@ int kern_main() {
 	init_serial();
 	init_idt();
 	init_palette();
-//	game_main();
+
+	struct Elf *elf;
+	struct Proghdr *ph, *eph;
+	unsigned char* pa, *i;
+
+	elf = (struct Elf*)106496;
+
+	readseg((unsigned char*)elf, 4096, 0);
+
+	ph = (struct Proghdr*)((uint8_t *)elf + elf->e_phoff);
+	eph = ph + elf->e_phnum;
+	for(; ph < eph; ph ++) {
+		pa = (unsigned char*)ph->p_pa; 
+		readseg(pa, ph->p_filesz, ph->p_offset); 
+		for (i = pa + ph->p_filesz; i < pa + ph->p_memsz; *i ++ = 0);
+	}
+
+	((void(*)(void))elf->e_entry)();
+
+	outw(0x8A00, 0x8A00);
+	outw(0x8A00, 0x8E00);
+	while(1);
 	return 0;
+}
+
+void waitdisk(void) {
+	while((inb(0x1F7) & 0xC0) != 0x40); 
+}
+
+void readsect(void *dst, int offset) {
+	waitdisk();
+	outb(0x1F2, 1);
+	outb(0x1F3, offset);
+	outb(0x1F4, offset >> 8);
+	outb(0x1F5, offset >> 16);
+	outb(0x1F6, (offset >> 24) | 0xE0);
+	outb(0x1F7, 0x20);
+
+	waitdisk();
+	insl(0x1F0, dst, SECTSIZE/4);
+}
+
+void readseg(unsigned char *pa, int count, int offset) {
+	unsigned char *epa;
+	epa = pa + count;
+	pa -= offset % SECTSIZE;
+	offset = (offset / SECTSIZE) + 1;
+	for(; pa < epa; pa += SECTSIZE, offset ++)
+		readsect(pa, offset);
 }
