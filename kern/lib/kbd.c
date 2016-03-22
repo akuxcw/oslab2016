397e6b06152d8b81d31becadb7e60cecf3359077
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-22 21:27:52 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:27:52 up  6:46,  8 users,  load average: 0.00, 0.18, 0.19 98949586f58edb18f09e325767d64fe9cc6131ce

--- kern/lib/kbd.c
+++
@@ -1,5 +0,0 @@
-#include <inc/syscall.h>
-
-void Update_kbd(int *kbd_state, int len) {
-	syscall(SYS_kbd, kbd_state, len);
-}
--- kern/lib/palette.c
+++
@@ -1,5 +0,0 @@
-#include <inc/syscall.h>
-
-void write_palette(char * L2_color_buffer) {
-	syscall(SYS_palette, L2_color_buffer);
-}
