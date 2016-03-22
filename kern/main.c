35d30f2c2dd4423ff0a1a30eb90c5981c3639b60
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-22 16:48:53 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:48:53 up  2:07,  7 users,  load average: 0.09, 0.09, 0.14 29f228fdb6fe59c9a50a9aa3a218fb19f1a02103

--- kern/main.c
+++ kern/main.c
@@ -34,7 +34,7 @@ int kern_main() {
 	for(; ph < eph; ph ++) {
 		pa = (unsigned char*)ph->p_pa; 
 		printk("%x %x %x\n", pa, ph->p_filesz, ph->p_offset);
-		readseg(pa, ph->p_filesz, ph->p_offset); 
+		readseg(pa, ph->p_filesz, 0x19000 + ph->p_offset); 
 		for (i = pa + ph->p_filesz; i < pa + ph->p_memsz; *i ++ = 0);
 	}
 
