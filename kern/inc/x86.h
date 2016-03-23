99d6071dfffc5fdc76b00d4c7ea560e4192cd475
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-23 19:14:28 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:14:28 up  3:06,  9 users,  load average: 0.12, 0.19, 0.16 1efa7e9631fa46583e0da7a3b9948af51bc0206

--- kern/inc/x86.h
+++ kern/inc/x86.h
@@ -255,10 +255,7 @@ read_esp(void)
 	return esp;
 }
 
-static inline void 
-pushl(uint32_t val) {
-	asm volatile("pushl %0" :: "r" (val));
-}
+#define pushl(val) asm volatile("pushl %0" :: "r" (val))
 
 static __inline void
 cpuid(uint32_t info, uint32_t *eaxp, uint32_t *ebxp, uint32_t *ecxp, uint32_t *edxp)
