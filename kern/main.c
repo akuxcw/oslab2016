bade2b342cd4950291044330a9de0d7cf5551d87
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-23 21:00:39 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:00:39 up  4:52,  9 users,  load average: 0.22, 0.21, 0.14 94c00f626c570f17488cf0e59653ec011f5b2850

--- kern/main.c
+++ kern/main.c
@@ -44,7 +44,9 @@ int kern_main() {
 
 	printk("Ready to game!\n");
 
-	
+	uint32_t eflags = read_eflags();
+	write_eflags(eflags | (1 << 14));
+
 	TrapFrame *tf = (TrapFrame *)0x10000;
 	tf->gs = tf->fs = tf->es = tf->ds = SEG_USER_DATA;
 	tf->eax = 0; tf->ebx = 1; tf->ecx = 2; tf->edx = 3;
