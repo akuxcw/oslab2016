8806c6764b4ac9bac8de633f36ae4a747dc9c2fb
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-24 20:26:21 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:26:21 up  4:23,  9 users,  load average: 0.01, 0.03, 0.08 b27119704b199e9b293b43aa29bef1566c837109

--- game/main.c
+++ game/main.c
@@ -2,13 +2,10 @@
 #include <time.h>
 void game();
 
-void init_kbd();
-
 int game_main(){
 //	asm volatile("sti");
 	time();
 	printf("@_@ Game Start!\n");
-	init_kbd();
 	game();
 	while(1);
 	return 0;
