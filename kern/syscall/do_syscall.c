b3c43d2cbe0ee4af18987299d106f10f3696723b
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-24 21:06:40 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:06:40 up  5:03,  9 users,  load average: 0.03, 0.10, 0.12 2fb275c23dcc5c3c7212ec89c08a52eb8cbe1454

--- kern/syscall/do_syscall.c
+++ kern/syscall/do_syscall.c
@@ -40,7 +40,7 @@ int query_key(int);
 
 static void sys_kbd(TrapFrame *tf) {
 	int i;
-	int *kbd = (int *)(char *)(tf->ebx + SEG_OFFSET);
+	int *kbd = (int *)(tf->ebx + SEG_OFFSET);
 	for(i = 0; i < tf->ecx; ++ i) {
 //		printk("%d %d\n", kbd[i], query_key(i));
 		if(kbd[i] == KEY_STATE_EMPTY && query_key(i) == KEY_STATE_PRESS) kbd[i] = KEY_STATE_PRESS; else
