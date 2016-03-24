0ff4bedde924875f61c328af0bc8b430ab4e2b93
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-24 21:19:14 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:19:14 up  5:16,  9 users,  load average: 0.39, 0.36, 0.23 344ae50a251b01d881faab533dcc60057db20d3c

--- kern/main.c
+++ kern/main.c
@@ -48,7 +48,7 @@ int kern_main() {
 
 	uint32_t eflags = read_eflags();
 
-	TrapFrame_iret *tf = (TrapFrame_iret *)0x100000;
+	TrapFrame_iret *tf = (TrapFrame_iret *)0x10000;
 	tf->gs = tf->fs = tf->es = tf->ds = SELECTOR_USER(SEG_USER_DATA);
 	tf->eax = 0; tf->ebx = 1; tf->ecx = 2; tf->edx = 3;
 	
