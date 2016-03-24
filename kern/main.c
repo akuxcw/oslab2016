cf4352689cdbb5a0a1a5b56ef29473cad09e58a6
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-24 18:16:44 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  18:16:44 up  2:13,  8 users,  load average: 0.05, 0.13, 0.10 5ea280b4def544b35adbc9a724cdefb620020156

--- kern/main.c
+++ kern/main.c
@@ -52,8 +52,8 @@ int kern_main() {
 	
 	tf->eflags = eflags | (1 << 9);
 	tf->eip = elf->e_entry + SEG_OFFSET;
-	tf->cs = SELECTOR_USER(SEG_USER_CODE);
-	tf->ss = SELECTOR_USER(SEG_USER_DATA);
+	tf->cs = (SEG_USER_CODE << 3) | 0x3;//SELECTOR_USER(SEG_USER_CODE);
+	tf->ss = (SEG_USER_CODE << 3) | 0x3;//SELECTOR_USER(SEG_USER_DATA);
 	tf->esp = 0x8000000;
 	asm volatile("movl %0, %%esp" : :"a"((int)tf));
 	asm volatile("popa");
