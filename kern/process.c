ed142343bf5a894862c7556726e9e27abc979604
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-25 20:06:54 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:06:54 up 12:05,  8 users,  load average: 0.16, 0.08, 0.12 b3143c04887f201ce812431aff7139385ce22c5b

--- kern/process.c
+++ kern/process.c
@@ -1,11 +1,13 @@
 #include <inc/process.h>
 #include <inc/list.h>
+#include <inc/stdio.h>
 
 PCB pcb[NR_PCB];
 ListHead *pcb_head;
 ListHead *unused_pcb;
 
 void init_process() {
+	printk("init_process\n");
 	list_init(pcb_head);
 	list_init(unused_pcb);
 	for(int i = 0; i < NR_PCB; ++ i) {
@@ -14,7 +16,7 @@ void init_process() {
 }
 
 PCB *new_process() {
-	if(list_empty(unused_pcb)) printk("Process full!");
+	if(list_empty(unused_pcb)) printk("Process full!\n");
 	ListHead *new_pcb = unused_pcb->next;
 	list_del(new_pcb);
 	list_add_after(new_pcb, pcb_head);
