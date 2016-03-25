8c4f365d88d382c045be712a8289c276ed77878d
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-25 10:59:56 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  10:59:56 up  2:58,  9 users,  load average: 0.00, 0.01, 0.10 66ecaf9d1b591174b8e597cb66bd3687704252a6

---
+++ kern/inc/process.h
@@ -0,0 +1,8 @@
+#ifndef KERN_INC_PROCESS_H
+#define KERN_INC_PRECESS_H
+
+#include <irq.h>
+
+//typedef 
+
+#endif
--- kern/irq/irq.h
+++ kern/irq/irq.h
@@ -13,6 +13,9 @@ typedef struct {
 } TrapFrame_iret;
 
 typedef struct TrapFrame {
+	uint32_t eip, cs, eflags;
+	uint32_t esp, ss;
+	uint32_t gs, fs, es, ds;
 	uint32_t edi, esi, ebp, old_esp, ebx, edx, ecx, eax;
 	int32_t irq;
 	uint32_t error_code;
