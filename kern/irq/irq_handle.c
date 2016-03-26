047a4f63762c98b49bd12f8728090e93865bdd23
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-26 14:33:37 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:33:37 up  4:43, 10 users,  load average: 0.05, 0.38, 1.90 7a491865a2f36629b71572019d89787a28bf6b9a

--- kern/irq/irq_handle.c
+++ kern/irq/irq_handle.c
@@ -17,13 +17,13 @@ irq_handle(TrapFrame *tf) {
 //	cliI();
 	int seg_tmp;
 	asm volatile("movl %%es, %0" : "=a"(seg_tmp) :);
-	offset = Get_gdt_off(seg_tmp >> 3);
 	asm volatile("movl %0, %%es\n\t"
 				 "movl %0, %%ds\n\t"
 				 "movl %0, %%fs\n\t"
 				 "movl %0, %%gs\n\t" 
 				 : 
 				 : "a"(SELECTOR_KERNEL(SEG_KERNEL_DATA)));
+	offset = Get_gdt_off(seg_tmp >> 3);
 	printk("%x\n", offset);
 	while(1);
 	uint32_t code, val;
