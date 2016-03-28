55b1ff758fa1444ab94e80769b1c6e6bfca84007
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-28 17:21:39 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:21:39 up  4:36,  9 users,  load average: 0.09, 0.08, 0.12 30f42e308b0e1f45c52ee133b916c5dfd7869208

--- kern/memory/segment.c
+++ kern/memory/segment.c
@@ -15,20 +15,30 @@ void init_memory() {
 	list_init(&free_seg);
 	list_init(&used_seg);
 	for(int i = 1; i < MAX_MEM/SEG_SIZE; i ++) {
+#ifdef USE_PAGE
+		seg[i].base = 0;
+		seg[i].limit = 0xFFFFF;
+#else
 		seg[i].base = i * SEG_SIZE;
-		seg[i].limit = 0x0;
+		seg[i].limit = 0x1FF;
+#endif
 		seg[i].gdt = i + 3;
 		list_add_before(&free_seg, &seg[i].list);
 	}
 }
 
 SegMan *mm_malloc(uint32_t vaddr, uint32_t size, uint32_t type) {
-//	if(list_empty(&free_seg))printk("Memory full!\n");
 	assert(!list_empty(&free_seg));
 	ListHead *new_seg = free_seg.next;
 	list_del(new_seg);
 	list_add_after(&used_seg, new_seg);
 	SegMan * tmp = list_entry(new_seg, SegMan, list);
-	set_segment(&gdt[tmp->gdt], DPL_USER, type, tmp->base - vaddr, tmp->limit);
+	uint32_t offset;
+#ifdef USE_PAGA
+	offset = 0;
+#else
+	offset = tmp->base - vaddr;
+#endif
+	set_segment(&gdt[tmp->gdt], DPL_USER, type, offset, tmp->limit);
 	return tmp;
 }
