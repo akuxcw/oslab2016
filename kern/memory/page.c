2be04eaac92d02d5d6709681f628dd397a690bfa
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-29 13:08:30 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  13:08:30 up  3:13,  9 users,  load average: 0.68, 1.48, 1.35 f69c8c64fa9eca098b312b7f4ec26e964499cb8

--- kern/memory/page.c
+++ kern/memory/page.c
@@ -64,12 +64,12 @@ void set_kern_page() {
 		ptable --;
 	}
 //	printk("***$$$$$$$$$***********\n");
+	while(1);
 	lcr3((uint32_t)pdir);
 /*	asm volatile("movl	%cr0, %eax\n\t"
 				 "orl	$0x80010001, %eax\n\t"
 				 "movl	%eax, %cr0\n\t");
 */	kern_init();
-	while(1);
 }
 
 //void start_() = (set_kern_page - KERNBASE);
