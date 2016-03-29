be5556f3c25f2f895a7e63797234c949c8b03af1
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-29 13:52:57 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  13:52:57 up  3:57,  9 users,  load average: 0.06, 0.08, 0.16 2432a69f7e32cbcff724689d936d8d7522baa8bc

--- kern/memory/page.c
+++ kern/memory/page.c
@@ -26,13 +26,13 @@ void set_kern_page() {
 
 //	memset(pdir, 0, NPDENTRIES * sizeof(pte_t));
 
-	while(1);
 	for (pdir_idx = 0; pdir_idx < MAX_MEM / PTSIZE; pdir_idx ++) {
 		pdir[pdir_idx] = (pde_t)ptable | 0x7;
 		pdir[pdir_idx + KERNBASE / PTSIZE] = (pde_t)ptable | 0x7;
 //		printk("%x\n", pdir[pdir_idx]);
 		ptable += NPDENTRIES;
 	}
+	while(1);
 	for(pdir_idx = 0xfc000000 / PTSIZE; pdir_idx < 0xfd000000 / PTSIZE; pdir_idx ++) {
 		pdir[pdir_idx] = (pde_t)ptable | 0x7;
 //		printk("%x\n", pdir[pdir_idx]);
