7eb7290af7be453408c4371a382f8f4cc1cb7c89
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-29 19:48:37 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:48:37 up  9:53,  9 users,  load average: 0.04, 0.06, 0.10 8428ba4ac2c210bcb394d45e73f7f11f8df3e29f

--- kern/memory/memory.c
+++ kern/memory/memory.c
@@ -41,7 +41,8 @@ uint32_t mm_malloc(uint32_t vaddr, uint32_t size, PCB* current) {
 	for(pdir_idx = vaddr / PTSIZE; pdir_idx < (vaddr + size + PTSIZE) / PTSIZE; ++ pdir_idx) {
 		if(pdir[pdir_idx] != 0x0) continue;
 		pdir[pdir_idx] = Get_free_pg() | 0x7;//(pde_t)va2pa(ptable) | 0x7;
-//		ptable += 1024;
+		printk("%x\n", pdir[pdir_idx]);
+		//		ptable += 1024;
 	}
 /*	for(i = current->ptable__; i < ptable; ++ i) {
 		*i = (pte_t)Get_free_pg() | 0x7;
