955277eb1e7407d44fabe6c7eca24f04bf26d75b
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-29 16:41:44 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:41:44 up  6:46,  9 users,  load average: 0.04, 0.11, 0.13 89a61b2beb74024fc63a47ee64700d2ff1e5ff8

--- kern/main.c
+++ kern/main.c
@@ -72,7 +72,7 @@ void load() {
 #else
 		pa = (unsigned char*)tmp[cnt]->base;
 #endif
-	//	printk("%x\n", pa);
+		printk("**********************\n");
 		readseg(pa, ph->p_filesz, OFFSET_IN_DISK + ph->p_offset); 
 		for (i = pa + ph->p_filesz; i < pa + ph->p_memsz; *i ++ = 0);
 	}
