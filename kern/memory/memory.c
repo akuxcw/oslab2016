37452b46e7cbe84a95bd43bc7572ed17d4a58b8b
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-30 16:25:40 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:25:40 up  1:11,  5 users,  load average: 0.04, 0.11, 0.13 a6acf0f1b1e4a85d3e525107df8bb951611f0c46

--- kern/memory/memory.c
+++ kern/memory/memory.c
@@ -61,7 +61,6 @@ uint32_t seg_alloc(uint32_t vaddr, PCB* current) {
 #endif
 	set_segment(&gdt[tmp->cs], DPL_USER, SEG_EXECUTABLE | SEG_READABLE, offset, tmp->limit);
 	set_segment(&gdt[tmp->ds], DPL_USER, SEG_WRITABLE, offset, tmp->limit);
-	//set_segment(&gdt[tmp->cs], DPL_USER, type, offset, tmp->limit);
 	current->tf.cs = SELECTOR_USER(tmp->cs);
 	current->tf.ds = current->tf.ss = current->tf.es =
 	current->tf.fs = current->tf.gs = SELECTOR_USER(tmp->ds);
@@ -74,7 +73,6 @@ uint32_t page_alloc(uint32_t vaddr, uint32_t size, PCB* current) {
 	for(pdir_idx = vaddr / PTSIZE; pdir_idx < (vaddr + size + PTSIZE) / PTSIZE; ++ pdir_idx) {
 		if(pdir[pdir_idx] != 0x0) continue;
 		pdir[pdir_idx] = Get_free_pg() | 0x7;
-//		readseg((const char *)(*(int *)(pdir[pdir_idx] & (~0x7))) & (~0x7), )
 //		printk("%x\n", pdir[pdir_idx]);
 	}
 	uint32_t pa = ((*(int *)(current->pdir[vaddr/PTSIZE] - 0x7)) - 0x7) + (vaddr & ((1 << 22) - 1));
