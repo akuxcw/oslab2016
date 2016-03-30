05a895e6cddf8fdd87835450bdaec88d8ffcf4a4
Auto_commit <141220012@smail.nju.edu.cn>
2016-03-30 16:30:18 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:30:18 up  1:15,  5 users,  load average: 0.18, 0.11, 0.13 fe72c008539f7a0ccfa1348a7c1ab58a6ff320f

--- kern/main.c
+++ kern/main.c
@@ -56,7 +56,9 @@ void load() {
 	struct Elf *elf;
 	struct Proghdr *ph, *eph;
 	unsigned char* pa, *i;
-
+#ifndef USE_PAGE
+	uint32_t vaddr;
+#endif
 	elf = (struct Elf*)(0x0019000);
 
 	readseg((unsigned char*)elf, 4096, OFFSET_IN_DISK);
@@ -75,7 +77,7 @@ void load() {
 #ifdef USE_PAGE
 		pa = (unsigned char *)page_alloc(ph->p_va, ph->p_memsz, current);
 #else
-		uint32_t vaddr = ph->p_va;
+		vaddr = ph->p_va;
 #endif
 //		printk("%x\n", OFFSET_IN_DISK + ph->p_offset);
 //		readprog(ph->p_va, ph->p_memsz, current, pa, OFFSET_IN_DISK + ph->p_offset);
@@ -100,7 +102,7 @@ void load() {
 	page_alloc(USER_STACK_TOP - USER_STACK_SIZE, USER_STACK_SIZE, current);
 //	printk("!!!!!\n");
 #else
-	tf->esp = 0x2000000 - pa + vaddr;
+	tf->esp = 0x2000000 - (int)pa + vaddr;
 #endif
 
 //	int j;
