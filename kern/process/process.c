963b8499babed0679d202808cdee2c676738e202
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-11 20:42:29 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:42:29 up  5:05, 10 users,  load average: 0.26, 0.22, 0.17 bbf9538082876a0ecf82490c30a44e29fab86257

--- kern/process/process.c
+++ kern/process/process.c
@@ -55,7 +55,7 @@ void ready(PCB *c) {
 }
 
 void sleep(PCB *c, uint32_t t) {
-		printk("%%%%%%%%%%%%%%\n");
+		//printk("%%%%%%%%%%%%%%\n");
 	list_del(&c->list);
 	c->time = t;
 	list_add_before(&Sleep, &c->list);
