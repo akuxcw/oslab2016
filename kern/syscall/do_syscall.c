25d7cffc3cce1d268f48658e793093096e455771
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-12 19:39:36 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:39:36 up  7:51, 10 users,  load average: 0.05, 0.14, 0.20 5bf5bf51b544816be8311c7dbfb3a50bc5d378d8

--- kern/syscall/do_syscall.c
+++ kern/syscall/do_syscall.c
@@ -73,7 +73,7 @@ void do_syscall(TrapFrame *tf) {
 		case SYS_palette: sys_palette(tf); break;
 		case SYS_kbd: sys_kbd(tf); break;
 		case SYS_time: tf->eax = Get_time(); break;
-		case SYS_sleep: printk("#");sleep(running_process(), tf->ebx); break;
+		case SYS_sleep: sleep(running_process(), tf->ebx); break;
 /*
 		case SYS_open : 
 			tf->eax = fs_open((char *)tf->ebx, tf->ecx); break;
