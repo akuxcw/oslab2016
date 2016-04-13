ed2bb09e490aa790ba805e9016e791bcd9e64c8b
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-13 18:33:01 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  18:33:01 up  5:53,  5 users,  load average: 0.04, 0.16, 0.19 507d8bb928d0ad17b27e29baddac4b12fadc06d2

--- game/main.c
+++ game/main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <time.h>
 void game();
-
-int game_main(){
+/*
+int game_main() {
 //	asm volatile("sti");
-/*	int addr = 0x4000;
+//	int addr = 0x4000;
 //	*(int *)addr = 1;
-	printf("****%d\n", *(int *)addr);
-*/
+//	printf("****%d\n", *(int *)addr);
+
 	sleep(100);
 	time();
 	printf("@_@ Game Start!\n");
 	game();
 	while(1);
+	return 0;
+}
+*/
+int game_main() {
+	while(1) {
+		printf("ping\n");
+		sleep(100);
+	}
 	return 0;
 }
