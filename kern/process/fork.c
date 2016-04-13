c4c61eb499046416181ae21981a1b00b2481bb9a
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-13 19:20:59 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:20:59 up  6:41,  8 users,  load average: 0.03, 0.07, 0.12 a8a11c134e51f44786e1b0959df1b337ab8fa75c

--- kern/process/fork.c
+++ kern/process/fork.c
@@ -23,8 +23,8 @@ int fork() {
 	for(i = 0; i < NPDENTRIES; ++ i) {
 		if((current->pdir[i] & PTE_P) && !(newp->pdir[i] & PTE_P)) {
 			newp->pdir[i] = Get_free_pg() | 0x7;
-			pa = (*(int *)(current->pdir[i] & (~ 0x7))) & (~ 0x7);
-			npa = (*(int *)(newp->pdir[i] & (~ 0x7))) & (~ 0x7);
+			pa = (*(int *)(current->pdir[i] -7/*& (~ 0x7)*/)) -7/*& (~ 0x7)*/;
+			npa = (*(int *)(newp->pdir[i] -7/*& (~ 0x7)*/)) -7/*& (~ 0x7)*/;
 			printk("%x %x\n", pa, npa);
 			memcpy((void *)npa, (void *)pa, PTSIZE);
 		}
