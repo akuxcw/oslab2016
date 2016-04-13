a44032994474fac81c53e4cefcdbbb86307cf2b9
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-13 19:22:11 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:22:11 up  6:42,  8 users,  load average: 0.16, 0.10, 0.13 9b89b6451e5b4edc4c7b96dcfb336209c63a9784

--- kern/memory/memory.c
+++ kern/memory/memory.c
@@ -38,7 +38,7 @@ uint32_t page_alloc(uint32_t vaddr, uint32_t size, PCB* current) {
 	for(pdir_idx = vaddr / PTSIZE; pdir_idx < (vaddr + size + PTSIZE) / PTSIZE; ++ pdir_idx) {
 		if(pdir[pdir_idx] != 0x0) continue;
 		pdir[pdir_idx] = Get_free_pg() | 0x7;
-		printk("%x %x\n", pdir[pdir_idx], *(int *)(pdir[pdir_idx] - 0x7));
+		printk("%x %x %x\n", pdir_idx, pdir[pdir_idx], *(int *)(pdir[pdir_idx] - 0x7));
 	}
 	uint32_t pa = ((*(int *)(current->pdir[vaddr/PTSIZE] - 0x7)) - 0x7) + (vaddr & ((1 << 22) - 1));
 	return pa;
--- kern/process/fork.c
+++ kern/process/fork.c
@@ -23,9 +23,9 @@ int fork() {
 	for(i = 0; i < NPDENTRIES; ++ i) {
 		if((current->pdir[i] & PTE_P) && !(newp->pdir[i] & PTE_P)) {
 			newp->pdir[i] = Get_free_pg() | 0x7;
-			pa = (*(int *)(current->pdir[i] -7/*& (~ 0x7)*/)) -7/*& (~ 0x7)*/;
-			npa = (*(int *)(newp->pdir[i] -7/*& (~ 0x7)*/)) -7/*& (~ 0x7)*/;
-			printk("%x %x\n", pa, npa);
+			pa = (*(int *)(current->pdir[i] & (~ 0x7))) & (~ 0x7);
+			npa = (*(int *)(newp->pdir[i] & (~ 0x7))) & (~ 0x7);
+			printk("%x %x %x\n", i, pa, npa);
 			memcpy((void *)npa, (void *)pa, PTSIZE);
 		}
 	}
