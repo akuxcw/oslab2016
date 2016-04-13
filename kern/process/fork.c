2d0f6e2b6c051ae36bc13078c357aacd64f630e9
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-13 19:06:04 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:06:04 up  6:26,  8 users,  load average: 0.25, 0.20, 0.16 6569a83fcd353263a3513c02e98236e8d2018b49

--- kern/process/fork.c
+++ kern/process/fork.c
@@ -16,7 +16,7 @@ int fork() {
 	}
 	lcr3(va2pa(idle.pdir));
 	for(i = 0; i < NPDENTRIES; ++ i) {
-		if(current->pdir[i] | PTE_P) {
+		if(current->pdir[i] & PTE_P) {
 			newp->pdir[i] = Get_free_pg() | 0x7;
 			pa = (*(int *)(current->pdir[i] & (~ 0x7))) & (~ 0x7);
 			npa = (*(int *)(newp->pdir[i] & (~ 0x7))) & (~ 0x7);
