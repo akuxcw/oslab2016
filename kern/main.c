8ceba23f720d6ce325dcb2bbe502e54603d5102b
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-13 20:00:32 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-67-generic #87~14.04.1-Ubuntu SMP Fri Mar 11 00:26:02 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:00:32 up  7:21, 11 users,  load average: 0.15, 0.15, 0.14 728f4a1dec04ba8fd6e7ddef7af9c5075328eb6a

--- kern/main.c
+++ kern/main.c
@@ -91,6 +91,7 @@ void load() {
 	uint32_t eflags = read_eflags();
 	
 	TrapFrame *tf = (TrapFrame *)current->kstack;
+	printk("tf = %x\n", *(int *)tf);
 	current->tf = tf;
 	tf->eax = 0; tf->ebx = 1; tf->ecx = 2; tf->edx = 3;
 	
