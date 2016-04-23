b37a36975deb9523cd37c7382dfd79fe8b77eae5
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-23 18:46:44 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  18:46:44 up  6:48,  8 users,  load average: 0.14, 0.17, 0.16 863b066ec2f8fd4524b4c6e3822ddafe440c4380

--- kern/memory/page.c
+++ kern/memory/page.c
@@ -125,7 +125,7 @@ uint32_t Get_free_pg() {
 }
 
 void Free_pg(int val) {
-	printk("%x %x\n", page[(val - va2pa(uptable))/0x1000 - 1].addr, val);
+	printk("%x %x %x\n", page[(val - va2pa(uptable))/0x1000 - 1].addr, va2pa(uptable), val);
 //	list_del(&page[].list);
 //	list_add_after(&free_pg, &val->list);
 }
