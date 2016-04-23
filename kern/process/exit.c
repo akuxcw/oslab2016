178a9762b76cf41cbe852e42218e5a5c8cce6678
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-23 18:50:23 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  18:50:23 up  6:51,  8 users,  load average: 0.13, 0.16, 0.16 98aefcd8ee168bffefebf214855507161f1d159c

--- kern/process/exit.c
+++ kern/process/exit.c
@@ -10,7 +10,7 @@ void exit(int x) {
 	del_process(current);
 	int i;
 	for(i = 0; i < NPDENTRIES; ++ i) {
-		if(current->pdir[i] & PTE_P) {
+		if(current->pdir[i] & PTE_P && current->pdir[i] & PTE_U) {
 			Free_pg(PTE_ADDR(current->pdir[i]));
 		}
 	}
