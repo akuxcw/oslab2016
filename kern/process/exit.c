7368b54046f390b7f3e187b4c73bad29ab98da05
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-23 19:05:44 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:05:44 up  7:07,  8 users,  load average: 0.18, 0.17, 0.16 9797e2c58d00a140e733e4ed7911e0ccbb15503b

--- kern/process/exit.c
+++ kern/process/exit.c
@@ -13,7 +13,7 @@ void exit(int x) {
 		if(current->pdir[i] & PTE_P && current->pdir[i] & PTE_U) {
 			Free_pg(PTE_ADDR(current->pdir[i]));
 		}
-		current->pdir[i] = 0;
+//		current->pdir[i] = 0;
 	}
 //	printk("%x %x\n", sizeof current->pdir, sizeof a);
 	memset(current, 0, sizeof current);
