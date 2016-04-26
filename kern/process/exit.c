580e29985536e8c7e908bd38184f04f6d2b95d11
Auto_commit <141220012@smail.nju.edu.cn>
2016-04-26 19:08:56 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:08:56 up  3:00,  6 users,  load average: 0.06, 0.09, 0.07 79688ff443397bc1aec48e415ddb6757d6402b8

--- kern/process/exit.c
+++ kern/process/exit.c
@@ -6,12 +6,12 @@
 #include <inc/memory.h>
 
 void exit(int x) {
-	printk("exit %x\n", current->pid);
+//	printk("exit %x\n", current->pid);
 	del_process(current);
 	int i;
 	for(i = 0; i < NPDENTRIES; ++ i) {
 		if((current->pdir[i] & PTE_P) && (current->pdir[i] & PTE_U)) {
-			printk("%x\n", PTE_ADDR(*(int *)PTE_ADDR(current->pdir[i])));
+//			printk("%x\n", PTE_ADDR(*(int *)PTE_ADDR(current->pdir[i])));
 			Free_pg(PTE_ADDR(current->pdir[i]));
 		}
 	}
