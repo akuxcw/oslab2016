ddb78f0bdecb3909d1c6dc4fff9c651688915055
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-08 20:39:20 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:39:20 up  2:02,  6 users,  load average: 0.14, 0.20, 0.22 4332a0d11eb07a6271354a15c7a722bca95e58df

--- kern/thread/thread.c
+++ kern/thread/thread.c
@@ -15,7 +15,7 @@ void thread_create(int *tid, int entry, int args) {
 //	newp->pdir[USER_STACK_TOP/PTSIZE - 1] = 0;
 	page_alloc(USER_STACK_TOP - USER_STACK_SIZE, USER_STACK_SIZE, current);
 	lcr3(va2pa(newp->pdir));
-	printk("%d\n", args);
+	printk("%d %x\n", args, newp->tf->esp);
 	*(int *)(newp->tf->esp + 0x4) = args;
 	lcr3(va2pa(current->pdir));
 	ready(newp);
