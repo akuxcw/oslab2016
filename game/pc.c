d218918fd877a00c0a6ae72499cfe733eeaf46fa
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-08 20:17:22 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:17:22 up  1:40,  6 users,  load average: 0.33, 0.19, 0.10 9dda4a5eb6cde3a540af43b430248457f48f7841

--- game/pc.c
+++ game/pc.c
@@ -1,14 +1,19 @@
 #include <thread.h>
 #include <stdio.h>
 
+int x;
+
 void c() {
-	printf("thread\n");
+	x ++;
+	printf("thread %d\n", x);
 	while(1);
 }
 
 int game_main() {
 	printf("PC problem.\n");
 	int id;
+	thread_create(&id, &c, NULL);
+	thread_create(&id, &c, NULL);
 	thread_create(&id, &c, NULL);
 	printf("main\n");
 	while(1);
