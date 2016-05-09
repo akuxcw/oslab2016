1219b0baef20e711bc4db97ffbb81c00b3310d3a
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-09 17:16:50 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  17:16:50 up  4:37,  5 users,  load average: 0.07, 0.14, 0.14 e4d0dfc2e4fede9b85316bc8ab81b8b62fc55e35

--- game/pc.c
+++ game/pc.c
@@ -1,31 +1,46 @@
 #include <thread.h>
 #include <stdio.h>
 #include <time.h>
+#define n 10
 
 int x;
-sem_t sem;
+sem_t mutex1, mutex2, full, empty;
+int buf[n], in = 0, out = 0;
+void p(void *args) {
+	while(1) {
+		sem_wait(empty);
+		sem_wait(mutex1);
+		buf[in] = (int) args;
+		in = (in + 1) % n;
+		sleep(100);
+		sem_post(mutex1);
+		sem_post(full);
+	}
+}
 
 void c(void *args) {
-	printf("start %d\n", (int)args);
-	sem_wait(sem);
-	x ++;
-//	sleep(10);
-	int y = 2 * x;
-	printf("thread %d %d %d\n", x, y, (int)args);
-	sleep(100);
-	sem_post(sem);
-	while(1);
+	while(1) {
+		sem_wait(full);
+		sem_wait(mutex2);
+		printf("%d %d\n", buf[out], (int)args);
+		out = (out + 1) % n;
+		sleep(200);
+		sem_post(mutex2);
+		sem_post(empty);
+	}
 }
 
 int game_main() {
 	printf("PC problem.\n");
 	int id;
-	sem = sem_open(1, 1);
+	mutex1 = sem_open(1, 1);
+	mutex2 = sem_open(1, 1);
+	full = sem_open(0, 0);
+	empty = sem_open(n, 0);
 	printf("main\n");
 	thread_create(&id, &c, (void *)1);
 	thread_create(&id, &c, (void *)2);
 	thread_create(&id, &c, (void *)3);
 	
-	sem_close(sem);
 	while(1);
 }
