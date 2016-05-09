85785595be89ee4d1fe96d25a950797a493b8f18
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-09 19:31:43 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:31:43 up  6:52,  5 users,  load average: 0.12, 0.14, 0.14 b3d387b11241b7a2e2a289313ff58d88ea10e189

--- game/pc.c
+++ game/pc.c
@@ -39,39 +39,39 @@ void p(void *args) {
 	int x = 0;
 	while(1) {
 		x ++;
-		printf("p 1 %d\n", (int)args);
+//		printf("p 1 %d\n", (int)args);
 		sem_wait(empty);
-		printf("p 2 %d\n", (int)args);
+//		printf("p 2 %d\n", (int)args);
 		sem_wait(mutex1);
-		printf("p 3 %d\n", (int)args);
+//		printf("p 3 %d\n", (int)args);
 		sleep(100);
-		printf("p 4 %d produce %d\n", (int)args, x);
+		printf("%d produce %d\n", (int)args, x);
 		buf[in] = x;
-		printf("p 5 %d\n", (int)args);
+//		printf("p 5 %d\n", (int)args);
 		in = (in + 1) % n;
-		printf("p 6 %d\n", (int)args);
+//		printf("p 6 %d\n", (int)args);
 		sem_post(mutex1);
-		printf("p 7 %d\n", (int)args);
+//		printf("p 7 %d\n", (int)args);
 		sem_post(full);
-		printf("p 8 %d\n", (int)args);
+//		printf("p 8 %d\n", (int)args);
 	}
 }
 
 void c(void *args) {
 	while(1) {
-		printf("c 1 %d\n", (int)args);
+//		printf("c 1 %d\n", (int)args);
 		sem_wait(full);
-		printf("c 2 %d\n", (int)args);
+//		printf("c 2 %d\n", (int)args);
 		sem_wait(mutex2);
-		printf("c 3 consume %d %d\n", buf[out], (int)args);
+		printf("%d consume %d\n", (int)args, buf[out]);
 		out = (out + 1) % n;
-		printf("c 4 %d\n", (int)args);
+//		printf("c 4 %d\n", (int)args);
 		sem_post(mutex2);
-		printf("c 5 %d\n", (int)args);
+//		printf("c 5 %d\n", (int)args);
 		sem_post(empty);
-		printf("c 6 %d\n", (int)args);
+//		printf("c 6 %d\n", (int)args);
 		sleep(200);
-		printf("c 7 %d\n", (int)args);
+//		printf("c 7 %d\n", (int)args);
 	}
 }
 
