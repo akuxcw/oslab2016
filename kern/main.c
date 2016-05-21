a213cf349957d7d175d7ad2ef68d3a7db0dc63ca
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-21 14:18:51 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:18:51 up  1:52,  6 users,  load average: 0.26, 0.36, 0.25 dc079039717a2262d02105e858f6b3e68c516e3f

--- kern/main.c
+++ kern/main.c
@@ -28,7 +28,7 @@ void load();
 uint8_t buf[4096];
 
 void testfs() {
-	ide_read(OFFSET_IN_DISK/SECTSIZE, buf, 1);
+	ide_read(OFFSET_IN_DISK, buf, 1);
 	printk("testfs\n");
 //	readseg(buf, 10, OFFSET_IN_DISK);
 	int i;
