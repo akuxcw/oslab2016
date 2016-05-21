bc76b604fb8b3f961c663f626d9313c2e4d7af92
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-21 20:11:27 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:11:27 up  7:44,  9 users,  load average: 0.09, 0.12, 0.10 5d0e8a1d8c23bbd1886cab8fd70f63aa60457706

--- kern/memory/memory.c
+++ kern/memory/memory.c
@@ -55,7 +55,7 @@ void readprog(uint32_t vaddr, uint32_t fsize, uint32_t msize, PCB *current, unsi
 	unsigned char *i;
 	for(pdir_idx = vaddr / PTSIZE; pdir_idx < (vaddr + msize - 1) / PTSIZE + 1; ++ pdir_idx) {
 		paddr = PTE_ADDR(*(int *)PTE_ADDR(current->pdir[pdir_idx]));
-//		printk("%x %x %x\n", current->pdir[pdir_idx], paddr, offset + (pdir_idx - vaddr / PTSIZE) * PTSIZE);
+		printk("%x %x %x\n", current->pdir[pdir_idx], paddr, offset + (pdir_idx - vaddr / PTSIZE) * PTSIZE);
 //		readseg((unsigned char *)(paddr), 
 //					PTSIZE, offset + (pdir_idx - vaddr / PTSIZE) * PTSIZE);
 		fseek(fd, (pdir_idx - vaddr / PTSIZE) * PTSIZE, SEEK_SET);
