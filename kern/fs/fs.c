93e90da1aec60f86e8c1726ba66b74d231ec8f27
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-21 19:22:16 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:22:16 up  6:55,  9 users,  load average: 0.17, 0.10, 0.07 792871d14643c234ef68054194333a874d2b411

--- kern/fs/fs.c
+++ kern/fs/fs.c
@@ -21,7 +21,7 @@ void testfs() {
 	int fd = fopen("1.txt", READ);	
 	fread(fd, buf, 600);
 //	ide_read(buf, 10, 3*SECTSIZE);
-	for(i = 0; i < 10; ++ i) printk("%d %c\n", i, buf[i]);
+	for(i = 0; i < 600; ++ i) printk("%d %c\n", i, buf[i]);
 	fd = fopen("2.txt", READ);	
 	fread(fd, buf, 5);
 //	ide_read(buf, 10, 3*SECTSIZE);
@@ -63,7 +63,7 @@ int fread(int fd, void *buf, size_t len){
 	while(len) {
 		int i = file[fd].offset / SECTSIZE;
 		int offset = tmp.index[i] * SECTSIZE + (file[fd].offset % SECTSIZE);
-		printk("%x\n", offset);
+//		printk("%x\n", offset);
 		int l = SECTSIZE - (file[fd].offset % SECTSIZE);
 		if(l > len) l = len;
 		ide_read(buf, l, offset);
