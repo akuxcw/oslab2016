12e56bc09ce154859049e392d1c73d8439762e46
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-24 20:51:20 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  20:51:20 up  7:11,  6 users,  load average: 0.50, 0.21, 0.40 58a7b1775b50a955c514d919f555fecd3e9686a7

--- game/fs.c
+++ game/fs.c
@@ -6,7 +6,7 @@ char buf[1024];
 int game_main() {
 	printf("Test file system.\n");
 	int fd = fopen("1.txt", READ);
-	fread(fd, buf, 10);
+	fread(fd, buf, 100);
 	printf("%s", buf);
 	while(1);
 	return 0;
