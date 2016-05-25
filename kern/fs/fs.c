893fbc68ee3802c0843ab60d98a65b5afd3941d6
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-25 19:10:08 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  19:10:08 up  6:44,  6 users,  load average: 0.11, 0.11, 0.13 ea45c3ddd9b456c1f32bbcca7b20670a2709b3e2

--- kern/fs/fs.c
+++ kern/fs/fs.c
@@ -58,25 +58,31 @@ int fopen(const char *pathname, int flag) {
 	return fp->fd;
 }
 
+int find_block(int fd) {
+	int i = file[fd].offset / SECTSIZE;
+	if(i < 12) {
+		return file[fd].ino.index[i];
+	} else {
+		if(i < 12 + 128) {
+			ide_read(index1, file[fd].ino.index[12], 1);
+			return index1[i - 12];
+		} else {
+			ide_read(index2, file[fd].ino.index[13], 1);
+			ide_read(index1, index2[(i - 12 - 128) / 128], 1);
+			return index1[i - 12 - 128];
+		}
+	}
+	return 0;
+}
+
 int fread(int fd, void *buf, size_t len){
 	if(file[fd].flag != READ) return -1;
 //	inode tmp;
 //	ide_read(&tmp, file[fd].inode, 1);
 	while(len) {
-		int i = file[fd].offset / SECTSIZE;
+		int i = find_block(fd);
 		if(i != file[fd].bufno) {
-			if(i < 12) {
-				ide_read(file[fd].buf, file[fd].ino.index[i], 1);
-			} else {
-				if(i < 12 + 128) {
-					ide_read(index1, file[fd].ino.index[12], 1);
-					ide_read(file[fd].buf, index1[i - 12], 1);
-				} else {
-					ide_read(index2, file[fd].ino.index[13], 1);
-					ide_read(index1, index2[(i - 12 - 128) / 128], 1);
-					ide_read(file[fd].buf, index1[i - 12 - 128], 1);
-				}
-			}
+			ide_read(file[fd].buf, i, 1);
 			file[fd].bufno = i;
 		}
 		int l = SECTSIZE - (file[fd].offset % SECTSIZE);
@@ -95,15 +101,15 @@ int fwrite(int fd, void *buf, size_t len) {
 //	inode tmp;
 //	ide_read(&tmp, file[fd].inode, 1);
 	while(len) {
-		int i = file[fd].offset / SECTSIZE;
+		int i = find_block(fd);
 		if(i != file[fd].bufno) {
-			ide_read(file[fd].buf, file[fd].ino.index[i], 1);
+			ide_read(file[fd].buf, i, 1);
 			file[fd].bufno = i;
 		}
 		int l = SECTSIZE - (file[fd].offset % SECTSIZE);
 		if(l > len) l = len;
 		memcpy(file[fd].buf + (file[fd].offset % SECTSIZE), buf, l);
-		ide_write(file[fd].buf, file[fd].ino.index[i], 1);
+		ide_write(file[fd].buf, i, 1);
 		file[fd].offset += l;
 		buf += l;
 		len -= l;
