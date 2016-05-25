76a3fc37d13fb4d7429df917b9e104cc69f444c4
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-25 18:19:29 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  18:19:29 up  5:54,  6 users,  load average: 0.11, 0.11, 0.13 7c9dede554c14d73aa81ba21ad86b4fe6eb65804

--- kern/fs/fs.c
+++ kern/fs/fs.c
@@ -44,6 +44,7 @@ void init_fs() {
 int fopen(const char *pathname, int flag) {
 	int i;
 	for(i = 0; i < DIR_FILES; ++ i) if(strcmp(pathname, root.entry[i].filename) == 0) break;
+	printk("%d %s\n", i, pathname);
 	assert(!list_empty(&file_head));
 	FCB * fp = list_entry(file_head.next, FCB, list);
 	list_del(&fp->list);
