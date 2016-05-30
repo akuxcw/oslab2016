53cce53a4c6731421177da8ee290151f5dd0e426
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-30 21:21:19 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:21:19 up  8:45,  5 users,  load average: 0.16, 0.13, 0.10 6e6a8f45d4d76c799e3398cc588e4c9a205c9f

--- game/fs.c
+++ game/fs.c
@@ -15,7 +15,7 @@ int game_main() {
 */
 	int fin = fopen("1.txt", READ);
 	int fout = fopen("2.txt", WRITE);
-	fread(fin, buf, 256);
+	fread(fin, buf, 257);
 	printf("%s", buf);
 	fwrite(fout, buf, 60);
 	int fin2 = fopen("2.txt", READ);
