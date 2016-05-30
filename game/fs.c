2884ce5f5b524bf64b75c1b4ac481733349d83e1
Auto_commit <141220012@smail.nju.edu.cn>
2016-05-30 21:17:49 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  21:17:49 up  8:41,  5 users,  load average: 0.03, 0.08, 0.09 e89c50353713c6b7e0cb726f79894a45c19abed

--- game/fs.c
+++ game/fs.c
@@ -5,11 +5,19 @@ char buf[1024];
 char a[] = "Hello FS.";
 int game_main() {
 	printf("Test file system.\n");
-	int fin = fopen("a/1.txt", READ);
+/*	int fin = fopen("a/1.txt", READ);
 	int fout = fopen("a/1.txt", WRITE);
 	fseek(fout, 10, SEEK_SET);
 	fwrite(fout, a, 9);
 	fread(fin, buf, 100);
+	printf("%s", buf);
+*/
+	int fin = fopen("1.txt", READ);
+	int fout = fopen("2.txt", WRITE);
+	fread(fin, buf, 600);
+	fwrite(fout, buf, 600);
+	int fin2 = fopen("2.txt", READ);
+	fread(fin2, buf, 600);
 	printf("%s", buf);
 	while(1);
 	return 0;
