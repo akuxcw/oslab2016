4dca1176b2a374fd8bbec9b852e8843b02bd2d8c
Auto_commit <141220012@smail.nju.edu.cn>
2016-06-01 16:30:19 +0800

    >  compile 141220012 akuxcw Linux akuxcw-Lenovo-M50-70 3.16.0-70-generic #90~14.04.1-Ubuntu SMP Wed Apr 6 22:56:34 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  16:30:19 up  3:44,  6 users,  load average: 0.08, 0.07, 0.11 62991128f501b6ad215287c58f31a8daf6cda5e5

--- kern/fs/block.c
+++ kern/fs/block.c
@@ -18,6 +18,7 @@ void init_blocks(uint8_t * map, uint32_t size) {
 	int i;
 	memset(zero, 0, sizeof zero);
 	list_init(&block_head);
+	list_init(&used_block);
 	for(i = 0; i < size / 512; ++ i) {
 		block[i].index = i;
 		if(map[i / 8] & (1 << (i & 0x7))) list_add_before(&used_block, &block[i].list);
